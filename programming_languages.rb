80e4c1e91de3dbbbbf83fd0c1edef98e0ca69771
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-06 13:33:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
   languages.each do |style, group|
     group.each do |language, type_hash|
       final_hash[language] = {}
-      final_hash[language] = {type_hash.keys => type_hash.values}
+      final_hash[language] = {type_hash.keys[0] => type_hash.values[0]}
       final_hash[language][:style] =[]
       final_hash[language][:style] << style
       binding.pry
